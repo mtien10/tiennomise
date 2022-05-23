7c13fc0d7a00cfd104cdc6844f6b5801d0c52a18
Manh Tien <tiennm@fabbi.io>
2022-05-23 17:56:08 +0700

    vue

--- product/views.py
+++ product/views.py
@@ -1,4 +1,5 @@
 from itertools import product
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,4 +10,17 @@ class LatestProductsList(APIView):
     def get(self, request, format=None):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+class ProductDetail(APIView):
+    def get_object(self, category_Slug, product_slug):
+        try:
+            return Product.objects.filter(category_slug=category_Slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_Slug, product_slug, format=None):
+        product = self.get_object(category_Slug, product_slug)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
