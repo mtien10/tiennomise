5b01c17e25e7511d748d47d15de892f705680e60
Manh Tien <tiennm@fabbi.io>
2022-05-27 17:50:52 +0700

    done

---
+++ tiennomise_vue/src/components/ProductBox.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="colomn is-3">
+        <div class="box">
+            <figure class="image mb-4">
+                <img :src="product.get_thumbnail">
+            </figure>  
+
+            <h3 class="is-size-4">{{ product.name }}</h3>
+            <p class="is-size-6 has-text-grey">{{ product.price }}đ</p>
+
+            <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+        </div>  
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductBox',
+    props: {
+        product: Object
+    }
+}
+</script>
+
+<style scoped>
+  .image {
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }  
+  .image img {
+      display: block;
+      height: 250px;
+      width: 100%;
+  }
+  .box {
+    margin-left: 15px;
+  }
+    
+</style>
--- tiennomise_vue/src/views/HomeView.vue
+++ tiennomise_vue/src/views/HomeView.vue
@@ -16,28 +16,19 @@
           <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
 
-      <div
-        class="colomn is-3"
+      <ProductBox 
         v-for="product in latestProducts"
         v-bind:key="product.id"
-      >
-        <div class="box">
-          <figure class="image mb-4">
-            <img :src="product.get_thumbnail">
-          </figure>  
-
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">{{ product.price }}đ</p>
-
-          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
-        </div>  
-      </div>
+        v-bind:product="product" />
     </div>
   </div>  
 </template>
 
 <script>
 import axios from 'axios'
+
+import ProductBox from '@/components/ProductBox'
+
 export default {
   name: 'Home',
   data() {
@@ -46,6 +37,7 @@ export default {
     }
   },
   components: {
+    ProductBox
   },
   mounted() {
     this.getLatestProducts()
@@ -73,19 +65,3 @@ export default {
 }
 </script>
 
-<style scoped>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }  
-  .image img {
-      display: block;
-      height: 250px;
-      width: 100%;
-  }
-  .box {
-    margin-left: 15px;
-  }
-    
-</style>
