905466fd0492a129865aa00f9c24d4ecfb22d0b6
Manh Tien <tiennm@fabbi.io>
2022-06-04 19:52:47 +0700

    add

--- tiennomise_vue/src/App.vue
+++ tiennomise_vue/src/App.vue
@@ -37,7 +37,16 @@
 
           <div class="navbar-item">
               <div class="buttons">
+                <template v-if="$store.state.isAuthenticated">
+                  <router-link to="/my-account" class="button is-light">My account</router-link>
+                </template>
+
+                <template v-else>
+                  <router-link to="/log-in" class="button is-light">Log in</router-link>
+                </template>
                 <router-link to="/log-in" class="button is-light">Log in</router-link>
+
+                
                 <router-link to="/cart" class="button is-success">
                   <span class="icon"><i class="fas fa-shopping-cart"></i></span>
                   <span>Cart ({{ cartTotalLength }})</span>
@@ -63,6 +72,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data(){
     return{
@@ -74,6 +84,14 @@ export default {
   },
   beforeCreate() {
     this.$store.commit('initializeStore')
+
+    const token = this.$store.state.token
+
+    if (token){
+      axios.defaults.headers.common['Authorization'] = "Token " + token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ""
+    }
   },
   mounted() {
     this.cart = this.$store.state.cart
--- tiennomise_vue/src/router/index.js
+++ tiennomise_vue/src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 import HomeView from '../views/HomeView.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/Login.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 const routes = [
   {
@@ -25,6 +29,24 @@ const routes = [
     component: Search
   },
   {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'Login',
+    component: LogIn
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta : {
+      requireLogin: true
+    }
+  },
+  {
     path: '/cart',
     name: 'Cart',
     component: Cart
@@ -44,6 +66,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
 })
 
 export default router
